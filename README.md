6b63910b0f278dd801c1c42888033cf2ce4246e4
Cagdas Caglak <cagdascaglak@gmail.com>
2016-12-24 21:25:40 +0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # AndroidThings-HMC5883L
-HMC5883L 3-axis magnetometer AndroidThings device driver
+HMC5883L 3-axis magnetometer AndroidThings device driver.
+
+###Usage
+You can check out my [ADXL345](https://github.com/cagdasc/AndroidThings-ADXL345) library. They have same structure.
+
+![device](device.png)
+
+License
+-------
+
+      Copyright 2016 Cagdas Caglak
+
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.
---
+++ sample/src/main/java/com/cacaosd/hmc5885lsample/BoardDefaults.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016 Cagdas Caglak
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cacaosd.hmc5885lsample;
+
+import android.os.Build;
+
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.util.List;
+
+/**
+ * Created by cagdas on 20.12.2016.
+ */
+
+@SuppressWarnings("WeakerAccess")
+public class BoardDefaults {
+    private static final String DEVICE_EDISON_ARDUINO = "edison_arduino";
+    private static final String DEVICE_EDISON = "edison";
+    private static final String DEVICE_RPI3 = "rpi3";
+    private static final String DEVICE_NXP = "imx6ul";
+    private static String sBoardVariant = "";
+
+    /**
+     * Return the preferred I2C port for each board.
+     */
+    public static String getI2CPort() {
+        switch (getBoardVariant()) {
+            case DEVICE_EDISON_ARDUINO:
+                return "I2C6";
+            case DEVICE_EDISON:
+                return "I2C1";
+            case DEVICE_RPI3:
+                return "I2C1";
+            case DEVICE_NXP:
+                return "I2C2";
+            default:
+                throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
+        }
+    }
+
+    private static String getBoardVariant() {
+        if (!sBoardVariant.isEmpty()) {
+            return sBoardVariant;
+        }
+        sBoardVariant = Build.DEVICE;
+        // For the edison check the pin prefix
+        // to always return Edison Breakout pin name when applicable.
+        if (sBoardVariant.equals(DEVICE_EDISON)) {
+            PeripheralManagerService pioService = new PeripheralManagerService();
+            List<String> gpioList = pioService.getGpioList();
+            if (gpioList.size() != 0) {
+                String pin = gpioList.get(0);
+                if (pin.startsWith("IO")) {
+                    sBoardVariant = DEVICE_EDISON_ARDUINO;
+                }
+            }
+        }
+        return sBoardVariant;
+    }
+}
